5eadd39d2752c852697d5509413c7ee513b83c89
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-07-21 02:39:29 +0300

    Update styling

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,37 +1,39 @@
 <template>
   <div class="flex flex-col items-center gap-6 w-full p-10">
     <!-- Welcome message -->
-    <div class="hero border">
+    <div class="hero">
       <div class="hero-content flex-col">
         <div>
           <h1 class="text-5xl font-bold">Ethereum account info viewer</h1>
-          <p class="py-6">Description text.</p>
+          <p class="py-6">
+            It can show all the data from account transactions, Ether and ERC-20
+            tokens balances at specified date.
+          </p>
         </div>
       </div>
     </div>
 
     <!-- Address input -->
-    <div class="border w-full justify-center">
+    <div class="w-full justify-center p-4">
       <div class="form-control">
-        <span class="label-text">
-          1. What is your Ethereum account address?</span
-        >
+        <span class="m-2">What is your Ethereum account address?</span>
         <input
           v-model="address"
           type="text"
           placeholder="0x0000000000000000000000000000000000000000"
-          class="input input-bordered"
+          class="input input-bordered m-2"
         />
       </div>
     </div>
 
     <!-- Use cases navigation -->
-    <div class="tabs border w-full justify-center">
+    <div class="tabs w-full justify-center p-4">
       <!-- Tabs header -->
       <NuxtLink
         :class="[
           'tab',
           'tab-lifted',
+          'text-xl',
           route.name == 'transactions' ? 'tab-active' : '',
         ]"
         to="/transactions"
@@ -41,6 +43,7 @@
         :class="[
           'tab',
           'tab-lifted',
+          'text-xl',
           route.name == 'ether-balance' ? 'tab-active' : '',
         ]"
         to="/ether-balance"
@@ -50,6 +53,7 @@
         :class="[
           'tab',
           'tab-lifted',
+          'text-xl',
           route.name == 'tokens-balance' ? 'tab-active' : '',
         ]"
         to="/tokens-balance"
@@ -58,7 +62,7 @@
     </div>
 
     <!-- Use case specific content -->
-    <div class="border w-full">
+    <div class="w-full p-4">
       <NuxtPage />
     </div>
   </div>
