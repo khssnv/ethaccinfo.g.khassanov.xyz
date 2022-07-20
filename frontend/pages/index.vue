cf0261459f39c436e644e2bb05b3c6b5aa65cfde
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-07-21 02:39:16 +0300

    Update index page message

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,6 @@
 <template>
-  <div>Index</div>
+  <div class="text-center">
+    Please paste your Ethereum mainnet address and select transactions, ether
+    balance or token balance view above.
+  </div>
 </template>
